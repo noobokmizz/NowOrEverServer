61a63bc9f9afd1e89f5107bd9ef75447e869a8a4
min sang <minsang0850@naver.com>
2021-04-19 14:11:32 +0900

    UI_v1.1-no connection, add color

---
+++ NowOrEver_UI/index.js
@@ -0,0 +1,10 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import App from './App';
+//import App from './Screen/Sample/PickerApp.js'
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => App);
