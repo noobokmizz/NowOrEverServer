d1fcf734d0d67637c83b98ef446deaec0f3b94d2
nooblette <min9hyuk@gmail.com>
2021-11-02 00:09:51 +0900

    update

--- src/main/java/noobokmizz/noworever/controller/BucketlistController.java
+++ src/main/java/noobokmizz/noworever/controller/BucketlistController.java
@@ -35,8 +35,6 @@ public class BucketlistController {
     @PostMapping("/bucketlist/show")
     @ResponseBody  // http의 응답 body부에 이 데이터를 직접 넣겠다(api를 통해 데이터를 바로 내리겠다).
     public Bucketlist.BucketlistMultipleContetns bucketlistRead(@RequestBody Bucketlist bucketlist){
-        System.out.println(bucketlist.getBk_id());
-        System.out.println(bucketlist.getMem_idnum());
         return bucketlistService.show(bucketlist);
     }
 
@@ -44,14 +42,6 @@ public class BucketlistController {
     @PostMapping(value = "/bucketlist/add")
     @ResponseBody
     public DefaultResponse bucketlistAdd(@RequestBody Bucketlist.BucketlistSingleConetents bucketlistSingleConetents){
-        System.out.println("mem idnum : '" + bucketlistSingleConetents.getMem_idnum() + "'");
-        System.out.println("bk_id : '" + bucketlistSingleConetents.getBk_id() + "'");
-        System.out.println(bucketlistSingleConetents.getBucketlistContents().getClass());
-        System.out.println("category : '" + bucketlistSingleConetents.getBucketlistContents().getCategory() + "'");
-        System.out.println("category_id : '" + bucketlistSingleConetents.getBucketlistContents().getCategory_id() + "'");
-        System.out.println("location : '" + bucketlistSingleConetents.getBucketlistContents().getLc_name() + "'");
-        System.out.println("lc_id : '" + bucketlistSingleConetents.getBucketlistContents().getLc_id() + "'");
-
         return new DefaultResponse(bucketlistService.put(bucketlistSingleConetents));
     }
 
@@ -59,13 +49,6 @@ public class BucketlistController {
     @PostMapping("/bucketlist/delete")
     @ResponseBody
     public DefaultResponse bucketlistDelete(@RequestBody Bucketlist.BucketlistMultipleContetns bucketlistMultipleContetns){
-        System.out.println("mem idnum : '" + bucketlistMultipleContetns.getMem_idnum() + "'");
-        System.out.println("bk_id : '" + bucketlistMultipleContetns.getBk_id() + "'");
-        System.out.println("category : '" + bucketlistMultipleContetns.getBucketlistContentsList().get(0).getCategory() + "'");
-        System.out.println("category_id : '" + bucketlistMultipleContetns.getBucketlistContentsList().get(0).getCategory_id() + "'");
-        System.out.println("location : '" + bucketlistMultipleContetns.getBucketlistContentsList().get(0).getLc_name() + "'");
-        System.out.println("lc_id : '" +bucketlistMultipleContetns.getBucketlistContentsList().get(0).getLc_id() + "'");
-
         return new DefaultResponse(bucketlistService.delete(bucketlistMultipleContetns));
     }
 
